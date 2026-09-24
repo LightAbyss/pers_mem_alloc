90bbea088e1e835d947a65c2a4d98e1f99f3c3c8
Juan Pablo Corredor <juan.corredorie@gmail.com>
2026-09-24 12:57:09 +0200

    Corrected markers naming

--- steps/s1/header_creation.c
+++ steps/s1/header_creation.c
@@ -5,14 +5,14 @@
 
 struct block_x {
     uint8_t marker;
-    struct free_area *prev;
+    struct block_x *prev;
     bool in_use;
     uint32_t lenght;
-    struct free_area *next;
+    struct block_x *next;
 };
 
 struct stats {
-    int magic_bytes;
+    uint8_t marker;
     bool simple_lock;
     uint32_t total_blocks;
     uint32_t total_pages;
@@ -21,39 +21,43 @@ struct stats {
 typedef struct stats my_stats;
 typedef struct block_x my_block;
 
-const int SAFE_GUARD = 0x55;
-const int HEADER_MARKER = 0x44;
+const uint8_t HEADER_MARKER = 0x55;
+const uint8_t BLOCK_MARKER = 0x44;
 const int PAGE_SIZE = 4096;
 const int BLOCK_SIZE = sizeof(my_block);
 
-char *heap_start = NULL;
+static char *heap_start = NULL;
 
 int main() {
     heap_start = sbrk(0);
-    sbrk(4096);
+    sbrk(PAGE_SIZE);
+    
     char *heap_end = sbrk(0);
-
     long int length = heap_end - heap_start;
-    *heap_start = 85;
+    
+    *(heap_start) = HEADER_MARKER;
 
     printf("Addr inicial: %p\n", heap_start);
     printf("Addr final: %p\n", heap_end);
     printf("Label heap_start: %d\n", (*heap_start));
     printf("Heap inicial size: %ld\n", length);
+    
     // Creamos el header
     my_stats *malloc_header = (my_stats *)heap_start;
     malloc_header->total_blocks = 1;
     malloc_header->total_pages = 1;
     // Creamos el primer bloque
     my_block *first_block = (my_block *)((char *)heap_start + sizeof(my_stats));
-    first_block->marker = HEADER_MARKER;
+    first_block->marker = BLOCK_MARKER;
     first_block->in_use = false;
     first_block->lenght = length - sizeof(my_stats) - sizeof(my_block);
     first_block->next = NULL;
     first_block->prev = NULL;
 
     printf("Label heap_start: %d\n", (*heap_start));
-    printf("Header pages: %d\tHeader blocks: %d\n", malloc_header->total_pages, malloc_header->total_blocks);
+    printf("Header label: %d\n", malloc_header->marker);
+    printf("Header lock: %d\n", malloc_header->simple_lock);
+    printf("Header pages: %d\t Header blocks: %d\n", malloc_header->total_pages, malloc_header->total_blocks);
     printf("First block size: %d\n", first_block->lenght);
     printf("First block marker: %d\n", first_block->marker);
   
