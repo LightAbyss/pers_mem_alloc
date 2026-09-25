61e37515358e504f0a47beb9880e7e4af2861cca
Juan Pablo Corredor <juan.corredorie@gmail.com>
2026-09-25 10:46:11 +0200

    Renamed main file

--- src/per_malloc.c
+++ src/per_malloc.c
@@ -1,26 +1,8 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <unistd.h>
-#include <stdbool.h>
 #include <assert.h>
 
-struct block_x {
-    uint8_t marker;
-    struct block_x *prev;
-    bool in_use;
-    uint32_t lenght;
-    struct block_x *next;
-};
-
-struct stats {
-    uint8_t marker;
-    bool simple_lock;
-    uint32_t total_blocks;
-    uint32_t total_pages;
-};
-
-typedef struct stats my_stats;
-typedef struct block_x my_block;
+#include "per_malloc.h"
 
 const uint8_t HEADER_MARKER = 0x55;
 const uint8_t BLOCK_MARKER = 0x44;
